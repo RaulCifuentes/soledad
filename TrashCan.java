d0866975b3c449cdea92cae879393691cf97e183
Raul <andres.cifuentes@gmail.com>
2015-10-28 22:12:45 -0300

    Dos

--- TrashCan.java
+++ TrashCan.java
@@ -1,6 +1,14 @@
 public class TrashCan {
 
+	public static TrashCan instance;
+
 	public static void main(String [] args){
 	 System.out.println('usando git');
 	}
+
+	public getInstance {
+		
+		return instance;
+	}
+
 }
